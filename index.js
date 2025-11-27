36bfd99ac23b13edb0cc887f277735965d159b42
rifatsamjid <rifattamim74328@gmail.com>
2025-11-27 08:50:27 +0600

    add the recent products api function

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const port = process.env.PORT || 4000
 app.use(cors())
 app.use(express.json())
 
-// 
-// 0AQi84c0MSIJUI7J
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.jkfjkqt.mongodb.net/?appName=Cluster1`;
 const client = new MongoClient(uri, {
@@ -32,23 +31,30 @@ async function run() {
         // add api
         app.post('/products', async (req, res) => {
             const newProduct = req.body
-            const result = await productsCollection.insertOne(newProduct)
+            const result = await productsCollection.insertMany(newProduct)
             res.send(result)
         })
 
         // api get
-        app.get('/products', async(req,res)=>{
+        app.get('/products', async (req, res) => {
             const cursor = productsCollection.find()
             const result = await cursor.toArray()
             res.send(result)
         })
 
         // api delete
-        app.delete('/products/:id',async (req,res)=>{
+        app.delete('/products/:id', async (req, res) => {
             const id = req.params.id;
-            const quarry = {_id: new ObjectId(id)}
+            const quarry = { _id: new ObjectId(id) }
             const result = await productsCollection.deleteOne(quarry)
             res.send(result)
+        })
+
+        // fresh products
+        app.get('/recent-products', async (req, res) => {
+            const cursor = productsCollection.find().sort({ date: -1 }).limit(6)
+            const result = await cursor.toArray()
+            res.send(result);
         })
 
         // await client.db("admin").command({ ping: 1 });
